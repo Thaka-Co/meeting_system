ead8b18ed9268aed9b032af6d65238dfddd422fa
amjadathathi <amjadathathi@gmail.com>
2022-02-21 12:48:11 +0300

    Added conntion to mongodb database

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,9 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import clientPromise from '../lib/mongodb'
 
-export default function Home() {
+
+export default function Home({isConnected}) {
   return (
     <div className={styles.container}>
       <Head>
@@ -10,6 +12,38 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
               <center>Hello World</center>
+
+                {isConnected ? (
+          <h2 >You are connected to MongoDB</h2>
+        ) : (
+          <h2 >
+            You are NOT connected to MongoDB. Check the <code>README.md</code>{' '}
+            for instructions.
+          </h2>
+        )}
+
     </div>
   )
+}
+
+
+
+
+export async function getServerSideProps(context) {
+  try {
+    // client.db() will be the default database passed in the MONGODB_URI
+    // You can change the database by calling the client.db() function and specifying a database like:
+    // const db = client.db("myDatabase");
+    // Then you can execute queries against your database like so:
+    // db.find({}) or any of the MongoDB Node Driver commands
+    await clientPromise
+    return {
+      props: { isConnected: true },
+    }
+  } catch (e) {
+    console.error(e)
+    return {
+      props: { isConnected: false },
+    }
+  }
 }
