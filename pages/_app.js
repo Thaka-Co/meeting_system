9830f280c1636ace0088d33fc8c0e47b93b53165
amjadathathi <amjadathathi@gmail.com>
2022-02-24 15:39:22 +0300

    create user data modle and meetings data model

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 import { ChakraProvider, extendTheme } from "@chakra-ui/react";
-import theme from "../config/theme";
+// import theme from "../config/theme";
 import { useRouter } from "next/router";
-import "react-calendar/dist/Calendar.css";
+// import "react-calendar/dist/Calendar.css";
 function MyApp({ Component, pageProps }) {
   const { locale } = useRouter();
   const direction = locale === "ar" ? "rtl" : "ltr";
