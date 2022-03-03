c87b1442a3165712bc2f97325f456f8e48c62dc9
NajlaHamdan <aznagla380@gmail.com>
2022-03-02 23:29:11 -0800

    get rooms

--- pages/api/votes/addVote.js
+++ pages/api/votes/addVote.js
@@ -1,14 +1,14 @@
 const voteModel = require("./../../models/Votes/votesModel");
 import addVote from "../../../lib/services/votes/addVote";
-const postVote = async (req, res) => {
+export default async function postVote (req, res)  {
   try {
     const { type, title, description, itemId } = req.body;
-    const newVote = new voteModel({
-      type,
-      title,
-      description,
-      itemId,
-    });
+    // const newVote = new voteModel({
+    //   type,
+    //   title,
+    //   description,
+    //   itemId,
+    // });
     if ((type, title, description, itemId)) {
       addVote(type, title, description, itemId);
     }
