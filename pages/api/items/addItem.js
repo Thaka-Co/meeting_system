ff4c1697f5c2121451a17a728381fce180ed8902
NajlaHamdan <aznagla380@gmail.com>
2022-03-05 20:53:35 -0800

    add item

--- pages/api/items/addItem.js
+++ pages/api/items/addItem.js
@@ -1,10 +1,12 @@
-import addRoom from "../../../lib/services/items/addItem";
+import addItem from "../../../lib/services/items/addItem";
 export default async function postItem(req, res) {
   try {
     const { title, description, userId, attachments, meetingId, isDelayed } =
       req.body;
-    if ((title, description, userId, attachments, meetingId, isDelayed)) {
-      addRoom(title, description, userId, attachments, meetingId, isDelayed);
+
+    if ((title, description)) {
+      console.log("add item", title, description);
+      addItem(title, description);
     } else {
       res.json("there is something wrong !!");
     }
