dedd149d0ee5f1d79af32173482dc471b38270a7
amjadathathi <amjadathathi@gmail.com>
2022-03-08 15:05:37 +0300

    fixed bugs for the build and depoy prosses

--- components/Comments/DisplayComments.js
+++ components/Comments/DisplayComments.js
@@ -1,14 +1,14 @@
 // @flow
 import * as React from "react";
 import { comments } from "../../Faker/general";
-import { Comment } from "./comment";
+import { Comment } from "./Comment";
 import { Text, Box, Input, useColorModeValue } from "@chakra-ui/react";
 export const DisplayComments = (props) => {
   return (
     <div>
       {comments.map((item, index) => {
         return (
-            <Comment key={index} name={item.name} value={item.value}/>
+          <Comment key={index} name={item.name} value={item.value} />
         );
       })}
     </div>
